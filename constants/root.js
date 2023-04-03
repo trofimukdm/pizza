f8469476dbc10b3761799bfe2037b1eac7c98845
DmitryTrofimuk <32343747+trofimukdm@users.noreply.github.com>
2023-04-04 00:29:09 +0300

    Add files via upload

---
+++ constants/root.js
@@ -0,0 +1,12 @@
+const ROOT_PRODUCTS = document.getElementById('products');
+const ROOT_HEADER = document.getElementById('header');
+const ROOT_SHOPPING = document.getElementById('shopping');
+const ROOT_SPINNER = document.getElementById('spinner');
+const ROOT_ERROR = document.getElementById('error');
+const ROOT_ABOUT = document.getElementById('about');
+const ROOT_MAIN = document.getElementById('main');
+const ROOT_404 = document.getElementById('page404');
+const ROOT_ORDER = document.getElementById('order');
+const ROOT_FOOTER = document.getElementById('footer');
+
+
