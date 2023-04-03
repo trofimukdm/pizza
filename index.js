230efdb61a5644967f2b56b8c973991a62604ab9
DmitryTrofimuk <32343747+trofimukdm@users.noreply.github.com>
2023-04-04 00:29:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+function render() {
+	const productsStore = localStorageUtil.getProducts();
+
+	headerPage.renderHeader(productsStore.length);
+	shoppingPage.renderShopping()
+	productsPage.renderProducts(productsStore.length);	
+	mainPage.renderMain();
+	footerPage.renderFooter();
+	aboutPage.renderAbout();
+}
+
+spinnerPage.renderSpinner();
+
+let CATALOG = [];
+
+// https://api.myjson.com/bins/jvsbu
+fetch('server/catalog.json')
+    .then(res => res.json())
+    .then(body => {
+		CATALOG = body;
+
+		setTimeout(() => {
+			spinnerPage.clearSpinner();
+			render();
+		}, 1000);
+    })
+    .catch(() => {
+        spinnerPage.clearSpinner();
+    	errorPage.renderError();
+			ROOT_SHOPPING.hidden = true;
+    })
